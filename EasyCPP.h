a9c4b78ec25cdfb88f4becab3248f8c4ad1d8349
Arex360 <49303647+Arex360@users.noreply.github.com>
2019-11-16 21:53:22 -0800

    Create EasyCPP.h

---
+++ EasyCPP.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<iostream>
+#include "module.h"
+#define Echo cout <<
+#define Input cin >> 
+#define use using
+#define echo namespace std ;
+#define stdlib std;
+#define start int main()
