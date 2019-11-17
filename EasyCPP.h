83d01cc15d79a68d1581ebba25263d5e5079966f
Arex360 <49303647+Arex360@users.noreply.github.com>
2019-11-16 22:31:55 -0800

    Update EasyCPP.h

--- EasyCPP.h
+++ EasyCPP.h
@@ -1,10 +1,15 @@
 #include<stdio.h>
 #include<iostream>
+#include "other/module1.h"
 #define Echo cout <<
 #define Input cin >> 
 #define use using
 #define echo namespace std ;
 #define stdlib std;
 #define start int main()
-#define startClass {
-#define endClass };
+#define $start {
+#define $end };
+#define $func public: void
+#define _func protected: void
+#define func void 
+#define _n <<
