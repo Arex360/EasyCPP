52fd283822fe3e39f7ad4a278f84924919b07867
Arex360 <49303647+Arex360@users.noreply.github.com>
2019-11-18 22:30:20 +0500

    Update export.h

--- other/export.h
+++ other/export.h
@@ -1,11 +1,11 @@
-#ifdef easy
-#define easy
+#ifdef easyCPP
+#define easyCPP
 #include<string>
 #include<iostream>
 #include "module1.h"
 #include "module2.h"
 #include "module3.h"
-#if defined EXPORT
+#ifdefined EXPORT
 #define exp __declspec(dllexport)
 #else
 #define exp __declspec(dllimport)
@@ -14,5 +14,9 @@ extern "C"{
    exp int sum(int a, int b);
    exp string isPrime(int num);
    exp int len(string a);
+   exp string type(int a);
+   exp string type(double a);
+   exp string type(string a);
+   exp string type(bool a);
 }
 #endif
